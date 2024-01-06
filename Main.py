68f07da0fd17609c2069e52a087e949f8d5b13ed
Vsevolod Pravdin <mr.freia@gmail.com>
2024-01-06 13:46:28 +0300

    add functions our_snake message gameLoop

--- Main.py
+++ Main.py
@@ -10,7 +10,42 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
-orange = (255, 165, 0)  # Оранжевый цвет
+orange = (255, 165, 0)
 
 dis_width = 800
 dis_height = 600
+
+dis = pygame.display.set_mode((dis_width, dis_height))
+
+snake_block = 20
+snake_speed = 10
+
+font_style = pygame.font.SysFont(None, 50)
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, orange, [x[0], x[1], snake_block, snake_block])
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = snake_block
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
+    foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
+
+    direction = 'RIGHT'
+
+

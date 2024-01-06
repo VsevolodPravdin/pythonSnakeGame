61d91e7f97b239fb205bcb5079f9ee39ce9445ad
Vsevolod Pravdin <mr.freia@gmail.com>
2024-01-06 13:17:18 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,16 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+orange = (255, 165, 0)  # Оранжевый цвет
+
+dis_width = 800
+dis_height = 600
